2ae41bd49dbc7c19ca7f8c704c94617029f01e91
Azerty <danila.94@mail.ru>
2024-06-24 18:33:34 +0300

    jpa func

--- src/main/java/ru/netology/hibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernate/repository/PersonRepository.java
@@ -13,24 +13,10 @@ import java.util.*;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Long, Contact> {
-    @PersistenceContext
-    EntityManager entityManager;
 
-    public List<Person> getPersonsByCity(String city) {
-        Query query = entityManager.createNamedQuery("select * from persons where city = city");
-        query.setParameter("city", city);
-        List<Person> personList = query.getResultList();
-
-        return personList;
-    }
-
-    public List<Person> getPersonsByAge() {
-        return personList;
-    }
-
-    public List<Person> getPersonsByNameAndSurname() {
-        return personList;
-    }
+    List<Person> findByCity(String city);
+    List<Person> findByAge(int age);
+    List<Person> findByNameAndSurname(String name, String surname);
 
 }
 
